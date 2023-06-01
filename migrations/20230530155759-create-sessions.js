1da11826e624b7577c0d232a9e3c394181ef3478
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-06-01 16:54:13 +0530

    Create 20230530155759-create-sessions.js

---
+++ migrations/20230530155759-create-sessions.js
@@ -0,0 +1,40 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Sessions", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      sessionName: {
+        type: Sequelize.STRING,
+      },
+      date: {
+        type: Sequelize.DATEONLY,
+      },
+      time: {
+        type: Sequelize.TIME,
+      },
+      venue: {
+        type: Sequelize.STRING,
+      },
+      playersNeeded: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Sessions");
+  },
+};
