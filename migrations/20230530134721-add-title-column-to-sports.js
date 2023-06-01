51ff6e2daf76febb08b556dcece31501d0e2c02c
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-06-01 17:46:51 +0530

    Update 20230530134721-add-title-column-to-sports.js

--- migrations/20230530134721-add-title-column-to-sports.js
+++ migrations/20230530134721-add-title-column-to-sports.js
@@ -2,10 +2,14 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // Step 1: Add the "title" column
     await queryInterface.addColumn('Sports', 'title', {
       type: Sequelize.STRING,
       allowNull: false,
     });
+
+    // Step 2: Update existing records with a valid value
+    await queryInterface.sequelize.query('UPDATE "Sports" SET "title" = \'Unknown\' WHERE "title" IS NULL;');
   },
 
   down: async (queryInterface, Sequelize) => {
