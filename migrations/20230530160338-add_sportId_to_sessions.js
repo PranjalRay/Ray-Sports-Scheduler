af9f25a56dba7b804964b45706e416bb706e440c
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-06-01 16:54:51 +0530

    Create 20230530160338-add_sportId_to_sessions.js

---
+++ migrations/20230530160338-add_sportId_to_sessions.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Sessions", "sportId", {
+      type: Sequelize.DataTypes.INTEGER,
+    });
+    await queryInterface.addConstraint("Sessions", {
+      fields: ["sportId"],
+      type: "foreign key",
+      references: {
+        table: "Sports",
+        field: "id",
+      },
+    });
+
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Sessions", "userId");
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
