455a4cb6140c788833637bf29af0ad00e37fa071
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-06-02 10:28:58 +0530

    Create 20230513041628-add-reason-in-sport-session.js

---
+++ migrations/20230513041628-add-reason-in-sport-session.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn("SportSessions","reason",{
+      type: Sequelize.DataTypes.STRING,
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
