3888aef6bf0e19cfa0ad7eddf1f99c4eafffa163
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-06-02 10:30:29 +0530

    Create cancelsession.js

---
+++ models/cancelsession.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class cancelSession extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      cancelSession.belongsTo(models.SportSession,{
+        foreignKey:"sessionId",
+      })
+      cancelSession.belongsTo(models.Sport,{
+        foreignKey:"sportId",
+      })
+      
+      
+    }
+
+    static async addReason({reason,sessionId,sportId}){
+      return this.create({reason:reason,sessionId:sessionId,sportId});
+    }
+
+
+  }
+  cancelSession.init({
+    reason: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'cancelSession',
+  });
+  return cancelSession;
+};
