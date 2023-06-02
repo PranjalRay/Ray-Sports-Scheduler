7ba73a1c6405b3f4a288bba60f06ab16b0d2b02b
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-06-02 10:31:56 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -9,29 +11,25 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Sport, {
-        foreignKey: "userId",
-      });
-      User.hasMany(models.Sessions, {
-        foreignKey: "userId",
-      });
-      User.hasMany(models.playerSessions, {
-        foreignKey: "player_id",
-      });
+      User.hasMany(models.Todo,{
+        foreignKey: 'userId'
+      })
+      User.hasMany(models.Sport,{
+        foreignKey: 'userId'
+      })
+      User.hasMany(models.SportSession,{
+        foreignKey: 'userId'
+      })
     }
   }
-  User.init(
-    {
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      role: DataTypes.STRING,
-      password: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: "User",
-    }
-  );
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
   return User;
 };
